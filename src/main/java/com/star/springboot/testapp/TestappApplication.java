cfa5b123cec92ffb8362ca3b6c8848ba7f3992d8
unknown <lklklnm@daum.net>
2020-07-30 04:09:18 +0900

    first commit

---
+++ src/main/java/com/star/springboot/testapp/TestappApplication.java
@@ -0,0 +1,13 @@
+package com.star.springboot.testapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/star/springboot/testapp/TestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.springboot.testapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

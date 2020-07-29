a0324b8e8969cad8cab74aa7376baa6692f45ddb
unknown <lklklnm@daum.net>
2020-07-30 04:46:44 +0900

    hello world

---
+++ src/main/java/com/star/springboot/testapp/TestController.java
@@ -0,0 +1,13 @@
+package com.star.springboot.testapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}

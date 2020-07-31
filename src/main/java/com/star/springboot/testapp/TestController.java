a3427bb49bfb7ba25ffd1fa5345d5bdddd704e74
unknown <lklklnm@daum.net>
2020-07-31 23:28:47 +0900

    post data

--- src/main/java/com/star/springboot/testapp/TestController.java
+++ src/main/java/com/star/springboot/testapp/TestController.java
@@ -1,13 +1,22 @@
 package com.star.springboot.testapp;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TestController {
 
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("msg", "input your id");
+        return "index";
+    }
+
+    @RequestMapping(value="/", method=RequestMethod.POST)
+    public String send(@RequestParam("id1") String id, Model model){
+        model.addAttribute("msg", "Hi. " + id + "!!");
+        model.addAttribute("id2", id);
         return "index";
     }
 }
